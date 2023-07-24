af913d9c7135d548176c233b20275378eae2ec24
demesameneshoa <demesameneshoa@gmail.com>
2023-07-25 00:13:31 +0300

    added bsic missions setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Rockets from './pages/Rockets';
+import Missions from './pages/Missions';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
       </Routes>
     </div>
   );
---
+++ src/pages/Missions.jsx
@@ -0,0 +1,10 @@
+const Missions = () => (
+  <div>
+    <h2>
+      Missions
+    </h2>
+
+  </div>
+);
+
+export default Missions;
