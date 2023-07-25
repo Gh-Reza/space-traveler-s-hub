66a11a4941792806f0e07ee1e836b7da7d4bffb5
demesameneshoa <demesameneshoa@gmail.com>
2023-07-25 16:21:47 +0300

    Render Missions list

--- src/pages/Missions.jsx
+++ src/pages/Missions.jsx
@@ -7,13 +7,58 @@ const Missions = () => {
   useEffect(() => {
     dispatch(fetchMissions());
   }, [dispatch]);
-  const missions = useSelector((state) => state.missions);
+  const missions = useSelector((state) => state.missions.missions);
+  const isLoading = useSelector((state) => state.missions.isLoading);
+  console.log(isLoading);
+  if (isLoading) {
+    return (
+      <div>
+        <table className="missions-table">
+          <tr>
+            <th>
+              Mission
+            </th>
+            <th>
+              Description
+            </th>
+            <th>
+              Status
+            </th>
+            <th>  </th>
+          </tr>
+          {Object.values(missions)
+            .flat()
+            .map((mission, index) => (
+              <tr key={Object.keys(missions)[index]}>
+                <td>
+                  {mission.mission_name}
+                </td>
+                <td>
+                  {mission.description}
+                </td>
+                <td>
+                  mission status
+                </td>
+                <td>
+                  <button type="button">Join Mission</button>
+                </td>
+              </tr>
+            ))}
+          ;
+        </table>
+        <p>
+          {isLoading}
+        </p>
+      </div>
+    );
+  }
+  return (
     <div>
       <h2>
-        Missions
+        Loadng...
       </h2>
-      <p>{missions}</p>
-    </div>;
+    </div>
+  );
 };
 
 export default Missions;
