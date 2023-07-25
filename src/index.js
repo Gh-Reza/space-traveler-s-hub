60b59c4bcb9d4534e747f9fdd8921dd42e992223
demesameneshoa <demesameneshoa@gmail.com>
2023-07-25 11:47:18 +0300

    resolve linters errors

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import './index.css';
-import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
