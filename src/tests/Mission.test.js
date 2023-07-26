9c70bc47cb3fa8fb1f040694b56903e646e54cd0
demesameneshoa <demesameneshoa@gmail.com>
2023-07-26 23:24:39 +0300

    Add test for mission page and mission unit test

---
+++ src/tests/Mission.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+import '@testing-library/jest-dom/extend-expect';
+import Mission from '../components/Mission';
+
+describe('Mission unit test', () => {
+  test('Mission Renders Properly ', () => {
+    const mockMission = {
+        mission_id: '323333',
+        mission_name: 'MockName',
+        description: 'Mock detail',
+        reserved: false,
+    };
+    const tree = renderer.create(
+      <Provider store={store}>
+        <Mission mission={mockMission} />
+      </Provider>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
--- src/tests/MissionsPage.test.js
+++ src/tests/MissionsPage.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
+import { screen, render } from '@testing-library/react';
 import store from '../redux/store';
 import '@testing-library/jest-dom/extend-expect';
 import Missions from '../pages/Missions';
@@ -9,5 +10,11 @@ describe('Mission Page unit test', () => {
   test('Mission Page Renders Properly ', () => {
     const tree = renderer.create(<Provider store={store}><Missions /></Provider>).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+  test('The Mission Page Contains Mission Table Headers', () => {
+    render(<Provider store={store}><Missions /></Provider>);
+    expect(screen.getByText('Mission')).toBeInTheDocument();
+    expect(screen.getByText('Description')).toBeInTheDocument();
+    expect(screen.getByText('Status')).toBeInTheDocument();
   });
 });
